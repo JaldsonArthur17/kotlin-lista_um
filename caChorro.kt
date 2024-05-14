cec00137638e4b114a06c61ba22d8efb5b6ac75c
Jaldson Arthur <126914358+JaldsonArthur17@users.noreply.github.com>
2024-05-14 13:34:24 -0300

    cachorro.kt

--- caChorro.kt
+++ caChorro.kt
@@ -1,4 +1,4 @@
-class cachorro (val nome: String, val raca: String, val idade: Int) {
+class cachorro(val nome: String, val raca: String, val idade: Int) {
 
 	fun latir(AuAu: String):String {
     return "AU AU!"
