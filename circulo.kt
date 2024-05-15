bee9a30de1cb5c82c786cc312b6b5a91717e08c6
Jaldson Arthur <126914358+JaldsonArthur17@users.noreply.github.com>
2024-05-15 18:17:49 -0300

    Update circulo.kt

--- circulo.kt
+++ circulo.kt
@@ -1,9 +1,9 @@
 class circulo(raio: Double){
 
-	fun calcArea(raio: Double):Double{
+	fun calcDiametro(raio: Double):Double{
         return 3.14 * raio
     } 
-    fun calcPerimetro(raio: Double):Double{
+    fun calcCircunferencia(raio: Double):Double{
         return 2 * 3.14 * raio
     }
 }
